0089578b9686784520e480a5bdde8234df694785
randyl <randyl@unity3d.com>
2019-01-21 21:16:28 -0800

    rust/: add a wrapper to struct

--- rust/channel.rs
+++ rust/channel.rs
@@ -9,10 +9,16 @@ struct Wrapper<'a> {
     input: &'a str,
 }
 
+// try to send something with a struct that has a lifetime
+#[derive(Clone)]
+struct WWrapper<'a> {
+    w: Wrapper<'a>,
+}
+
 // main function
 fn main() {
     // create a mspc for the struct
-    let (tx, rx): (Sender<Wrapper>, Receiver<Wrapper>) = mpsc::channel();
+    let (tx, rx): (Sender<WWrapper>, Receiver<WWrapper>) = mpsc::channel();
 
     // start the send loop
     thread::spawn(move || {
@@ -24,16 +30,21 @@ fn main() {
 }
 
 // function to send stuff into queue often
-fn send_loop(tx: Sender<Wrapper>) {
+fn send_loop(tx: Sender<WWrapper>) {
     let input = Wrapper {
         input: "message",
     };
+
+    let input_WWrapper = WWrapper {
+        w: input,
+    };
+
     let five_seconds = Duration::new(5, 0);
 
     // start the send loop
     loop {
         // Make a struct
-        let pass_in = input.clone();
+        let pass_in = input_WWrapper.clone();
 
         // send the struct into the channel
         tx.send(pass_in).unwrap();
@@ -43,9 +54,9 @@ fn send_loop(tx: Sender<Wrapper>) {
 }
 
 // function to continuously read stuff from
-fn read_loop(rx: Receiver<Wrapper>) {
+fn read_loop(rx: Receiver<WWrapper>) {
     // This is an infinite loop.
     for received in rx {
-        println!("Got: {}", received.input);
+        println!("Got: {}", received.w.input);
     }
 }
