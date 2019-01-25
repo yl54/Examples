6129365eedebb50f0ce27b033428406fa19e33da
randyl <randyl@unity3d.com>
2019-01-25 14:47:19 -0800

    scala/: clean up jinja renderer code

--- scala/src/main/scala/JinjaRenderer.scala
+++ scala/src/main/scala/JinjaRenderer.scala
@@ -1,7 +1,6 @@
 package scala
 
-import java.util.{HashMap, Map}
-
+import java.util.{Map}
 
 import com.hubspot.jinjava.Jinjava
 import com.hubspot.jinjava.interpret.JinjavaInterpreter
@@ -11,24 +10,11 @@ import com.hubspot.jinjava.tree.Node
 
 // object for renderer runner
 object JinjaRenderer {
-    def renderTemplateFile(file: String): String = {
+    def renderTemplateFile(template: String, context: Map[String, String]): String = {
         // Create a renderer object
-
-        // Set the variables necessary to render into the file
+        val jinjava = new Jinjava()
 
         // Render the variables into the file
-
-        // Get the output of that rendered stuff
-
-        val jinjava = new Jinjava()
-        var context = new HashMap[String, String]()
-        context.put("name", "Jared")
-        context.put("dest", "Alaska")
-
-        val template = "{{ name }}: {{ dest }}"
-
-        val renderedTemplate = jinjava.render(template, context)
-
-        return renderedTemplate
+        return jinjava.render(template, context)
     }
 }
--- scala/src/test/scala/JinjaRendererTest.scala
+++ scala/src/test/scala/JinjaRendererTest.scala
@@ -1,11 +1,21 @@
 package scala
 
+import java.util.{HashMap}
+
 import org.scalatest.{ FunSuite }
 
 class JinjaRendererTest extends FunSuite {
     test("check renderTemplateFile returns expected output") {
+        // Get the file contents to render into
+        val template = "{{ name }}: {{ dest }}"
+
+        // Set the variables necessary to render into the file
+        var context = new HashMap[String, String]()
+        context.put("name", "Jared")
+        context.put("dest", "Alaska")
+
         val expected: String = "Jared: Alaska"
-        val actual = JinjaRenderer.renderTemplateFile("file")
+        val actual = JinjaRenderer.renderTemplateFile(template, context)
         assert(expected == actual)
     }
 }
