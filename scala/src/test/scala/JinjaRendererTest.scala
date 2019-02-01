35ab8f32cf38ef7bd44f652a7c7d755bf62560f2
randyl <randyl@unity3d.com>
2019-02-01 12:56:12 -0800

    scala/: add test case for jinja or keyword

--- scala/src/test/scala/JinjaRendererTest.scala
+++ scala/src/test/scala/JinjaRendererTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.{ FunSuite }
 class JinjaRendererTest extends FunSuite {
     test("check renderTemplateFile returns expected output") {
         // Get the file contents to render into
-        val template = "{{ name }}: {{ dest }}"
+        val template = "{{name}}: {{dest}}"
 
         // Set the variables necessary to render into the file
         var context = new HashMap[String, String]()
@@ -16,6 +16,22 @@ class JinjaRendererTest extends FunSuite {
 
         val expected: String = "Jared: Alaska"
         val actual = JinjaRenderer.renderTemplateFile(template, context)
+        assert(expected == actual)
+    }
+
+    test("check renderTemplateFile works for keyword or") {
+        // Get the template
+        val template = "{{name or ''}}: {{dest or '\"\"'}}: game: {{game or 'sm'}}: thing: {{thing or ''}}" 
+
+        // Set the necessary variables
+        var context = new HashMap[String, String]()
+        context.put("name", "Jared")
+
+        val expected: String = "Jared: \"\": game: sm: thing: "
+        val actual = JinjaRenderer.renderTemplateFile(template, context)
+
+        println(expected)
+        println(actual)
         assert(expected == actual)
     }
 }
