5d502b5799f5e542658a52836cc7af21e66a20b8
randyl <randyl@unity3d.com>
2019-03-07 16:20:26 -0800

    scala/: add a getPriorDaysList function

--- scala/src/main/scala/BasicScala.scala
+++ scala/src/main/scala/BasicScala.scala
@@ -1,7 +1,28 @@
 package scala
 
+import java.time.{ Instant, LocalDate, ZoneOffset }
+import java.time.format.DateTimeFormatter
+
 object BasicScala {
     def SeqToString(list: Seq[String], delimiter: String): String = {
         list.mkString(delimiter)
+    }
+
+    val DateHourFormat: DateTimeFormatter = DateTimeFormatter ofPattern "yyyy-MM-dd HH"
+    val dateFormat: DateTimeFormatter     = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    def getPriorDaysList(numPriorDays: Int,
+                         processDateStr: String): List[String] = {
+        // Get the calendar instance of the process date
+        val processDate = LocalDate.parse(processDateStr, dateFormat)
+
+        val priorDaysList = (0 to numPriorDays).map(processDate.minusDays(_)).toList
+        priorDaysList.map(dt => {
+            getDateString(dt)
+        })
+    }
+
+    def getDateString(processDate: LocalDate): String = {
+        processDate.format(dateFormat)
     }
 }
--- scala/src/test/scala/BasicScalaTest.scala
+++ scala/src/test/scala/BasicScalaTest.scala
@@ -16,4 +16,22 @@ class BasicScalaTest extends FunSuite {
         // expected == actual
         assert(expected == actual)
     }
+
+    // test to check if the date formatter returns the right list
+    test("check getPriorDaysList works") {
+        // define an amount of days
+        val days = 4
+
+        // define a process day
+        val processDate = "2019-02-03"
+
+        // expected
+        val expected = List("2019-02-03", "2019-02-02", "2019-02-01", "2019-01-31", "2019-01-30")
+
+        // actual
+        val actual = BasicScala.getPriorDaysList(days, processDate)
+
+        // assert
+        assert(expected === actual)
+    }
 }
