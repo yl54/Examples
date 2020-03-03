8a1b15b1e94236a597aaf840997ab63f7453986b
randyl <randyl@unity3d.com>
2020-03-03 14:29:47 -0800

    add initial java gradle project files

---
+++ java/src/main/java/GrpcClient.java
@@ -0,0 +1,21 @@
+// This class is for a gRPC library.
+
+// Imports
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class GrpcClient {
+
+    // Function to create a new gRPC client
+
+    // Function to make a quick protobuf call
+
+    // Function to check for expected response of protobuf call
+}
---
+++ java/src/test/java/GrpcClientTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(JUnit4.class)
+public class GrpcClientTest {
+    @Test
+    public void testSanityCheck() {
+        assertEquals(2, 2);
+    }
+
+    // Test to do a simple grpc client thing
+        // Define the host and port
+
+        // Create a managed channel
+
+        // Create a client
+
+
+        // Make a call to the service
+
+        // Close the connection
+    
+}
