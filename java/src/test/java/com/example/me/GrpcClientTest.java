93983c2f3b1d56fd388d9307548b548b5eae1ff5
randyl <randyl@unity3d.com>
2020-03-03 16:17:24 -0800

    move test to make it work

---
+++ java/src/test/java/com/example/me/GrpcClientTest.java
@@ -0,0 +1,44 @@
+package com.example.me;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertEquals;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+@RunWith(JUnit4.class)
+public class GrpcClientTest {
+    // private final GreeterGrpc.GreeterBlockingStub blockingStub;
+
+    @Test
+    public void testSanityCheck() {
+        assertEquals(2, 2);
+    }
+
+    // Test to do a simple grpc client thing
+    @Test
+    public void testGrpcCallGranular() {
+        // Define the host and port
+        String user = "world";
+        // Access a service running on the local machine on port 50051
+        String target = "localhost:50051";
+
+        // Create a communication channel to the server, known as a Channel. Channels are thread-safe
+        // and reusable. It is common to create channels at the beginning of your application and reuse
+        // them until the application shuts down.
+        ManagedChannel channel = ManagedChannelBuilder.forTarget(target)
+                // Channels are secure by default (via SSL/TLS). For the example we disable TLS to avoid
+                // needing certificates.
+                .usePlaintext()
+                .build();
+
+        // Create a client
+
+        // Make a call to the service
+
+        // Close the connection
+    }
+}
